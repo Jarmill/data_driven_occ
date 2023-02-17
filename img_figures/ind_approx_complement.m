7437d0cdbcf9042542ecab2fbb75f851be0c349f
Jared Miller <jared.fr.miller@gmail.com>
2023-02-16 19:33:45 -0500

    indicator under-approximation

--- img_figures/ind_approx_complement.m
+++ img_figures/ind_approx_complement.m
@@ -1,6 +1,6 @@
-function [out] = ind_approx(d, box, XT)
-%IND_APPROX approximate the indicator function on the sets XT
-%from above, yielding an outer-approximation
+function [out] = ind_approx_complement(d, box, XT)
+%IND_APPROX_COMPLEMENT approximate the indicator function on the sets XT
+%from below, yielding an inner approximation
 %univariate
 x = sdpvar(1, 1);
 
@@ -17,19 +17,31 @@ for i = 1:Nset
     XT_cell{i} = struct('ineq', (x-XT(i, 1))*(XT(i,2)-x), 'eq', []);
 end
 
+% XTC = [[box(1), XT]; [XT, box(2)]];
+XTC = [[box(1); XT(:, 2)], [XT(:, 1); box(2)]];
+XTC_cell = cell(Nset+1, 1);
+% XT_cell = cell(Nset, 1);
+for i = 1:Nset+1
+    XTC_cell{i} = struct('ineq', (x-XTC(i, 1))*(XTC(i,2)-x), 'eq', []);
+end
+
 leb = LebesgueBoxMom( d, box', 1);
 
 %form polynomials
 [w, cw] = polynomial(x, d);
 
-objective = leb'*cw;
+objective = -leb'*cw;
 
-[p0, cons0, coeff0] = constraint_psatz(w, X, x, d);     
-cons = [cons0:'Nonneg'];
-coeff = [cw; coeff0];
+cons = [];
+coeff = [cw];
 
+for i = 1:Nset+1
+    [p0, cons0, coeff0] = constraint_psatz(-w, XTC_cell{i}, x, d);     
+    cons = [cons; cons0:['Region Comp.', num2str(i)]];
+    coeff = [coeff; coeff0];
+end
 for i = 1:Nset
-    [pcurr, conscurr, coeffcurr] = constraint_psatz(w-1, XT_cell{i}, x, d);
+    [pcurr, conscurr, coeffcurr] = constraint_psatz(1-w, XT_cell{i}, x, d);
     cons = [cons; conscurr:['Region ', num2str(i)]];
     coeff = [coeff; coeffcurr];
 end
@@ -51,7 +63,7 @@ out.residual = residual;
 [cwe,mwe] = coefficients(w,x);
 out.w = value(cwe)'*mwe;                 
 out.w_eval = polyval_func(out.w, x);           
-out.volume = value(objective);
+out.volume = -value(objective);
 
 
 
--- img_figures/ind_approx_test.m
+++ img_figures/ind_approx_test.m
@@ -12,12 +12,15 @@ XT = [0.1, 0.5;
 
 % dlist = [10, 100];
 
-dlist = [6, 20, 120];
+% dlist = [6, 20, 120];
+dlist = 200;
+% dlist = [6, 20];
+% dlist = 10;
 
-
-out = cell(length(dlist),1);
+out = cell(length(dlist),2);
 for i = 1:length(dlist)
-    out{i} = ind_approx(dlist(i), X, XT);
+    out{i, 1} = ind_approx(dlist(i), X, XT);
+    out{i, 2} = ind_approx_complement(dlist(i), X, XT);
 end
 
 
@@ -36,7 +39,23 @@ plot(Xx, Xy, 'k', 'LineWidth', 6)
 xlabel('$x$', 'interpreter', 'latex', 'fontsize', 14)
 ylabel('$\omega(x)$', 'interpreter', 'latex', 'fontsize', 14)
 for i = 1:length(dlist)
-    wsample = arrayfun(out{i}.w_eval, xsample);
+    wsample = arrayfun(out{i, 1}.w_eval, xsample);
     plot(xsample, wsample, 'color', colors(i, :), 'LineWidth',3)
 end
 hold off
+title('Indicator Over-Approximation', 'fontsize', 16)
+
+figure(6)
+clf
+hold on
+Xx = [0; 0.1;0.1;0.5;0.5;0.8;0.8;0.9;0.9; 1];
+Xy = [0;  0;   1;  1;  0;  0;  1;  1;  0; 0]; 
+plot(Xx, Xy, 'k', 'LineWidth', 6)
+xlabel('$x$', 'interpreter', 'latex', 'fontsize', 14)
+ylabel('$\tilde{\omega}(x)$', 'interpreter', 'latex', 'fontsize', 14)
+for i = 1:length(dlist)
+    wsample = arrayfun(out{i, 2}.w_eval, xsample);
+    plot(xsample, wsample, 'color', colors(i, :), 'LineWidth',3)
+end
+hold off
+title('Indicator Under-Approximation', 'fontsize', 16)
